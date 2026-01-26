574533352bc921ca370fd93f6613773a299689b5
Tevin Temu <tevintemu@gmail.com>
2026-01-26 21:19:43 +0300

    compile and build with published package

--- workflows/mara_north_event_report/ecoscope-workflows-mara-north-event-report-workflow/ecoscope_workflows_mara_north_event_report_workflow/dags/jupytext.py
+++ workflows/mara_north_event_report/ecoscope-workflows-mara-north-event-report-workflow/ecoscope_workflows_mara_north_event_report_workflow/dags/jupytext.py
@@ -123,7 +123,7 @@ from ecoscope_workflows_ext_mnc.tasks import (
 from ecoscope_workflows_ext_mnc.tasks import map_column_values as map_column_values
 from ecoscope_workflows_ext_mnc.tasks import map_name_values as map_name_values
 from ecoscope_workflows_ext_mnc.tasks import merge_dataframes as merge_dataframes
-from ecoscope_workflows_ext_mnc.tasks import merge_multiple_df as merge_multiple_df_1
+from ecoscope_workflows_ext_mnc.tasks import merge_multiple_df as merge_multiple_df
 from ecoscope_workflows_ext_mnc.tasks import pivot_df as pivot_df
 from ecoscope_workflows_ext_mnc.tasks import (
     remove_brackets_from_column as remove_brackets_from_column,
@@ -12543,7 +12543,7 @@ merge_trajs_params = dict()
 
 
 merge_trajs = (
-    merge_multiple_df_1.set_task_instance_id("merge_trajs")
+    merge_multiple_df.set_task_instance_id("merge_trajs")
     .handle_errors()
     .with_tracing()
     .partial(
